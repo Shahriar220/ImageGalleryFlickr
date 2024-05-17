5ff9d56e288438961d99de30a18aeaec4ac9e8d9
Shahriar220 <shahriar.uchchash@gmail.com>
2024-05-17 10:26:37 +0600

    navigation graph added, home screen initial setup done,splash screen added

--- app/src/main/java/com/example/imagegallery/MainActivity.kt
+++ app/src/main/java/com/example/imagegallery/MainActivity.kt
@@ -3,27 +3,30 @@ package com.example.imagegallery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.imagegallery.components.AppNavigationGraph
 import com.example.imagegallery.ui.theme.ImageGalleryTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             ImageGalleryTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.White)
+                ) {
+                    AppEntryPoint()
                 }
             }
         }
@@ -31,17 +34,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ImageGalleryTheme {
-        Greeting("Android")
-    }
+fun AppEntryPoint() {
+    AppNavigationGraph()
 }
---
+++ app/src/main/java/com/example/imagegallery/screens/HomeScreen.kt
@@ -0,0 +1,13 @@
+package com.example.imagegallery.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+/**
+ * @author Shahriar
+ * @since ১৭/৫/২৪ ১০:০২ AM
+ */
+@Composable
+fun HomeScreen() {
+    Text(text = "Home Screen")
+}
