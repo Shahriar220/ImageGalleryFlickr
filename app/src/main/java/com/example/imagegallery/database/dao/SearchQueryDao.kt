9b382f42a743f70ae060227253861464e419ca67
Shahriar220 <shahriar.uchchash@gmail.com>
2024-05-20 20:50:16 +0600

    search in bottom sheet added //wip

--- app/src/main/java/com/example/imagegallery/database/dao/SearchQueryDao.kt
+++ app/src/main/java/com/example/imagegallery/database/dao/SearchQueryDao.kt
@@ -20,4 +20,7 @@ interface SearchQueryDao {
 
     @Query("SELECT * FROM search_table WHERE `query` = :query LIMIT 1")
     suspend fun getSearchQuery(query: String): SearchEntity?
+
+    @Query("Delete From search_table where id=:id")
+    fun deleteFromDb(id: Int)
 }
